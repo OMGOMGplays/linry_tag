d9bfb2d0c4690a318f82fb708de37550cc513aed
OMGOMGplays <loke@jenjen.se>
2021-08-23 20:23:53 +0200

    Added
    Yeah

---
+++ code/MinimalHud.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox.UI;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace MinimalExample
+{
+	public partial class MinimalHudEntity : Sandbox.HudEntity<RootPanel>
+	{
+		public MinimalHudEntity()
+		{
+			if ( IsClient )
+			{
+				RootPanel.SetTemplate( "/minimalhud.html" );
+			}
+		}
+	}
+
+}
