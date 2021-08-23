2f862f2eb8a2137929a11e5dffb81779c9c31442
OMGOMGplays <loke@jenjen.se>
2021-08-23 19:22:31 +0200

    Initial Commit
    Added everything and what not, you already know that though, right?

---
+++ code/Hud.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox.UI;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace Example
+{
+	public partial class HudEntity : Sandbox.HudEntity<RootPanel>
+	{
+		public HudEntity()
+		{
+			if ( IsClient )
+			{
+				RootPanel.SetTemplate( "/hud.html" );
+			}
+		}
+	}
+
+}
